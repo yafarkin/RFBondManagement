1c5e5b4bb74475eb56d7c8089e5e3d1fa2cb8ab5
Evgeny Yafarkin <yafarkin@gmail.com>
2021-07-15 17:25:20 +0400

    Technical commit;

---
+++ RfBondManagement.WinForm/Controls/DisplayPaperListUC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RfFondPortfolio.Common.Interfaces;
+
+namespace RfBondManagement.WinForm.Controls
+{
+    public partial class DisplayPaperListUC : UserControl
+    {
+        protected IPaperRepository _paperRepository;
+
+        public DisplayPaperListUC()
+        {
+            InitializeComponent();
+        }
+
+        public void InitDI(IPaperRepository paperRepository)
+        {
+            _paperRepository = paperRepository;
+        }
+
+
+    }
+}

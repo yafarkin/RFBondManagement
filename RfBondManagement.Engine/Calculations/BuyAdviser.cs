7a693e14d3ffa599815c0877bb014f036f4720cc
Evgeny Yafarkin <yafarkin@gmail.com>
2021-12-21 23:42:07 +0400

    Start implementing buy adviser (based on portfolio structure) - for helping in UI and in backtesting code

---
+++ RfBondManagement.Engine/Calculations/BuyAdviser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using RfFondPortfolio.Common.Dtos;
+using RfFondPortfolio.Common.Interfaces;
+
+namespace RfBondManagement.Engine.Calculations
+{
+    public class BuyAdviser
+    {
+        public static async Task<IEnumerable<PortfolioAction>> Advise(ILogger logger, PortfolioEngine engine, long availSum, bool useVa, DateTime? onDate, IExternalImport import, IHistoryRepository historyRepository)
+        {
+            var result = new List<PortfolioAction>();
+
+            var portfolio = engine.Portfolio;
+            var rootLeaf = portfolio.RootLeaf;
+            var flattenPapers = FlattenPaperStructure(rootLeaf, 1);
+            var paperPrices = new Dictionary<string, decimal>(flattenPapers.Count);
+
+            foreach (var paper in flattenPapers)
+            {
+                if (null == onDate)
+                {
+                    var price = await import.LastPrice(logger, paper.Paper);
+                    paperPrices.Add(paper.Paper.SecId, price.Price);
+                }
+                else
+                {
+                    var historyPrice = historyRepository.GetNearHistoryPriceOnDate(paper.Paper.SecId, onDate.Value);
+                    paperPrices.Add(paper.Paper.SecId, historyPrice.LegalClosePrice);
+                }
+            }
+
+            throw new NotImplementedException();
+
+            //var content = engine.Build();
+            //await engine.FillPrice(content, onDate);
+
+            //while (availSum > 0)
+            //{
+            //    var maxDisablanced = FindMaxDisablancedPaper(portfolio, content, rootLeaf);
+
+            //}
+
+            return result;
+        }
+
+        public static IList<PortfolioStructureLeafPaper> FlattenPaperStructure(PortfolioStructureLeaf entryLeaf, decimal percent)
+        {
+            var result = new List<PortfolioStructureLeafPaper>();
+
+            if (entryLeaf == null)
+            {
+                return result;
+            }
+
+            var totalVolume =
+                  (entryLeaf.Papers?.Sum(x => x.Volume) ?? 0)
+                + (entryLeaf.Children?.Sum(x => x.Volume) ?? 0);
+
+            if (entryLeaf.Papers != null)
+            {
+                foreach (var paper in entryLeaf.Papers)
+                {
+                    var paperLocalPercent = paper.Volume / totalVolume;
+                    var paperGlobalPercent = paperLocalPercent * percent;
+                    result.Add(new PortfolioStructureLeafPaper
+                    {
+                        Paper = paper.Paper,
+                        Volume = paperGlobalPercent
+                    });
+                }
+            }
+
+            if (entryLeaf.Children != null)
+            {
+                foreach (var child in entryLeaf.Children)
+                {
+                    var childLocalPercent = child.Volume / totalVolume;
+                    var childGlobalPercent = childLocalPercent * percent;
+
+                    result.AddRange(FlattenPaperStructure(child, childGlobalPercent));
+                }
+            }
+
+            return result;
+        }
+
+        private static PortfolioStructureLeafPaper FindMaxDisablancedPaper(Portfolio portfolio, PortfolioAggregatedContent content, PortfolioStructureLeaf entryLeaf)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RfBondManagement.UnitTests/BuyAdviserTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using RfBondManagement.Engine.Calculations;
+using RfFondPortfolio.Common.Dtos;
+using Shouldly;
+
+namespace RfBondManagement.UnitTests
+{
+    [TestFixture]
+    public class BuyAdviserTests
+    {
+        [Test]
+        public void FlattenStructure_CorrectPercentCalc_Test()
+        {
+            var portfolio = new Portfolio
+            {
+                RootLeaf = new PortfolioStructureLeaf
+                {
+                    Children = new List<PortfolioStructureLeaf>
+                    {
+                        new PortfolioStructureLeaf
+                        {
+                            Name = "ChildLeaf1",
+                            Volume = 100,
+                            Papers = new List<PortfolioStructureLeafPaper>
+                            {
+                                new PortfolioStructureLeafPaper
+                                {
+                                    Paper = new SharePaper { SecId = "Paper1" },
+                                    Volume = 100
+                                },
+                                new PortfolioStructureLeafPaper
+                                {
+                                    Paper = new SharePaper { SecId = "Paper2" },
+                                    Volume = 200
+                                }
+                            }
+                        },
+                        new PortfolioStructureLeaf
+                        {
+                            Name = "ChildLeaf2",
+                            Volume = 100,
+                            Papers = new List<PortfolioStructureLeafPaper>
+                            {
+                                new PortfolioStructureLeafPaper
+                                {
+                                    Paper = new SharePaper { SecId = "Paper3" },
+                                    Volume = 30
+                                },
+                                new PortfolioStructureLeafPaper
+                                {
+                                    Paper = new SharePaper { SecId = "Paper4" },
+                                    Volume = 20
+                                }
+                            },
+                            Children = new List<PortfolioStructureLeaf>
+                            {
+                                new PortfolioStructureLeaf
+                                {
+                                    Name = "ChildLeaf3",
+                                    Volume = 50,
+                                    Papers = new List<PortfolioStructureLeafPaper>
+                                    {
+                                        new PortfolioStructureLeafPaper
+                                        {
+                                            Paper = new SharePaper { SecId = "Paper5" },
+                                            Volume = 100
+                                        },
+                                        new PortfolioStructureLeafPaper
+                                        {
+                                            Paper = new SharePaper { SecId = "Paper6" },
+                                            Volume = 50
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var flattenList = BuyAdviser.FlattenPaperStructure(portfolio.RootLeaf, 1);
+            flattenList.Count.ShouldBe(6);
+
+            flattenList[0].Paper.SecId.ShouldBe("Paper1");
+            flattenList[0].Volume.ShouldBe(0.1666m, 0.0001m);
+
+            flattenList[1].Paper.SecId.ShouldBe("Paper2");
+            flattenList[1].Volume.ShouldBe(0.3333m, 0.0001m);
+
+            flattenList[2].Paper.SecId.ShouldBe("Paper3");
+            flattenList[2].Volume.ShouldBe(0.15m);
+
+            flattenList[3].Paper.SecId.ShouldBe("Paper4");
+            flattenList[3].Volume.ShouldBe(0.1m);
+
+            flattenList[4].Paper.SecId.ShouldBe("Paper5");
+            flattenList[4].Volume.ShouldBe(0.1666m, 0.0001m);
+
+            flattenList[5].Paper.SecId.ShouldBe("Paper6");
+            flattenList[5].Volume.ShouldBe(0.0833m, 0.0001m);
+
+        }
+    }
+}
